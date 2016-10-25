fe17ee57e9f00660e4eee0e67ab188ec1376e6de
Andreych1500 <Savitskuy@ukr.net>
2016-10-25 11:22:40 +0300

    new version

--- modules/apply/main.php
+++ modules/apply/main.php
@@ -60,7 +60,7 @@ if(isset($_GET['cron']) && $_GET['cron'] == 'ok' && $_SERVER['REMOTE_ADDR'] == C
         }
 
         $idCard = implode($idCard, ',');
-
+        
         $history = q("
             SELECT `fileScan`
             FROM `admin_educational_history`
